3cf492db6fb40ea22a2905f0d929a04b039bf939
Luka Tchelidze <157907877+Null-logic-0@users.noreply.github.com>
2026-01-14 18:24:27 +0400

    Add Card and MainScreen components
    Introduces a reusable Card component with header, content, footer, title, and description subcomponents, styled using Tailwind CSS. Also adds a MainScreen component that displays a fullscreen hero image with an overlay and a centered Card for maintenance messaging.

---
+++ src/app/components/MainScreen.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Image from "next/image";
+import heroImage from "../../../public/hero-food.png";
+import { Card, CardContent } from "./Card";
+
+function FullScreenHero() {
+  return (
+    <main className="h-screen flex justify-center items-center">
+      <div className="absolute inset-0">
+        <Image
+          src={heroImage}
+          fill
+          alt="Georgian cuisine"
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 bg-linear-to-t from-[#ff6b36]/40 via-black/50 to-background" />
+      </div>
+      <Card className="fixed mx-auto w-full max-w-xl">
+        <CardContent>
+          <p className="text-lg font-medium p-6 text-gray-600  mx-auto text-center">
+            We’re performing some updates. Thanks for your patience — we’ll be
+            back online soon!
+          </p>
+          <hr className="border-gray-400" />
+        </CardContent>
+      </Card>
+    </main>
+  );
+}
+
+export default FullScreenHero;
